07486dbfc24814812d14e323b2f8a2bf66962590
Ali Darrudi <ali.daroudi@gmail.com>
2026-01-31 16:49:14 +0330

    get-delete-post-patch tried

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { GetUserDTO } from './dto/get-users.dto';
 import * as userModel from './user.model';
 import { UsersService } from './users.service';
 
@@ -7,12 +19,36 @@ export class UsersController {
   constructor(private userService: UsersService) {}
 
   @Get()
-  userList() {
+  userList(@Query() getUserDto: GetUserDTO): userModel.User[] {
+    if (Object.keys(getUserDto).length)
+      return this.userService.find(getUserDto);
     return this.userService.userList();
   }
 
+  @Get('/:id')
+  findUser(@Param('id') id: string): userModel.User {
+    const user = this.userService.findUser(id);
+    if (!user) {
+      throw new NotFoundException(`User with id ${id} not found`);
+    }
+    return user;
+  }
+
   @Post()
-  addUser(@Body('name') name): userModel.User {
-    return this.userService.addUser(name);
+  addUser(@Body() createUserDTO: CreateUserDTO): userModel.User {
+    return this.userService.addUser(createUserDTO);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: string) {
+    this.userService.deleteUser(id);
+  }
+
+  @Patch('/:id')
+  updateUser(
+    @Param('id') id: string,
+    @Body('name') name: string,
+  ): userModel.User {
+    return this.userService.updateUser(id, name);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,20 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { GetUserDTO } from './dto/get-users.dto';
 import { User } from './user.model';
 
 @Injectable()
 export class UsersService {
   private users: User[] = [];
-  userList() {
+  userList(): User[] {
     return this.users;
   }
 
-  addUser(name: string) {
+  find(getUserDto: GetUserDTO): User[] {
+    let users = this.userList();
+    const { name, email } = getUserDto;
+    if (name) users = users.filter((item) => item.name == getUserDto.name);
+    else if (email)
+      users = users.filter((item) => item.email == getUserDto.email);
+    if (!users) {
+      throw new NotFoundException(`users not found`);
+    }
+
+    return users;
+  }
+
+  addUser(createUserDTO: CreateUserDTO) {
+    const { name, email } = createUserDTO;
     const user: User = {
       id: randomUUID(),
-      name: name,
+      name,
+      email,
     };
     this.users.push(user);
+    return user;
+  }
+
+  findUser(userId: string) {
+    return this.users.find((item) => item.id == userId);
+  }
+
+  deleteUser(id: string) {
+    const index = this.users.findIndex((item) => item.id == id);
+    this.users.splice(index, 1);
+  }
+
+  updateUser(id: string, name: string): User {
+    let user = this.findUser(id);
+    if (user) user.name = name;
+    if (!user) {
+      throw new NotFoundException(`user not found`);
+    }
     return user;
   }
 }
